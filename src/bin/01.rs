ef469019cc0426394ad2794711fb21c3a7d04fab
Guillaume <guillaume@pelikan-mobility.com>
2025-12-01 12:32:52 +0100

    Small cleanups

---
+++ src/bin/01.rs
@@ -0,0 +1,22 @@
+use aoc2025::*;
+
+fn main() -> anyhow::Result<()> {
+    let mut cur: i32 = 50;
+    let mut num_zero = 0;
+    let mut num_pass_zero = 0;
+    for l in lines("data/example01.txt")? {
+        let incr = match l.chars().next() {
+            Some('R') => 1,
+            Some('L') => -1,
+            _ => anyhow::bail!("bad line {l:?}"),
+        };
+        for _ in 0..l[1..].parse()? {
+            cur = (cur + incr).rem_euclid(100);
+            num_pass_zero += u32::from(cur == 0);
+        }
+        num_zero += u32::from(cur == 0);
+    }
+    println!("Part1: {}", num_zero);
+    println!("Part2: {}", num_pass_zero);
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use std::fs::File;
 use std::io::{self, BufRead};
 
-pub fn lines(file: &str) -> anyhow::Result<impl Iterator<Item = io::Result<String>>> {
-    Ok(std::io::BufReader::new(std::fs::File::open(file)?).lines())
+pub fn lines(file: &str) -> io::Result<Vec<String>> {
+    io::BufReader::new(File::open(file)?).lines().collect()
 }
