59181aa475faee69d9227bd2889146faa297447a
Guillaume <guillaume@pelikan-mobility.com>
2025-12-01 10:52:10 +0100

    Cleanup day 1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,26 +1,19 @@
-use std::io::{self, BufRead};
+use aoc2025::*;
 
 fn main() -> anyhow::Result<()> {
     let mut cur = 50;
     let mut nb_zero = 0;
     let mut nb_pass_zero = 0;
-    for l in io::BufReader::new(std::fs::File::open("data/example1.txt")?).lines() {
+    for l in lines("data/example1.txt")? {
         let l = l?;
-        let n = l[1..].parse::<i32>()?;
-        match l.chars().next() {
-            Some('R') => {
-                for _ in 0..n {
-                    cur = (cur + 1) % 100;
-                    nb_pass_zero += i32::from(cur == 0);
-                }
-            }
-            Some('L') => {
-                for _ in 0..n {
-                    cur = (cur + 100 - 1) % 100;
-                    nb_pass_zero += i32::from(cur == 0);
-                }
-            }
+        let incr = match l.chars().next() {
+            Some('R') => 1,
+            Some('L') => -1,
             _ => anyhow::bail!("bad line {l}"),
+        };
+        for _ in 0..l[1..].parse()? {
+            cur = (cur + 100 + incr) % 100;
+            nb_pass_zero += i32::from(cur == 0);
         }
         nb_zero += i32::from(cur == 0);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+use std::io::{self, BufRead};
 
+pub fn lines(file: &str) -> anyhow::Result<impl Iterator<Item = io::Result<String>>> {
+    Ok(std::io::BufReader::new(std::fs::File::open(file)?).lines())
+}
