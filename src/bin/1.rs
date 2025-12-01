7d3fdb1b72b6f3961fa3b5def3868db059a657a3
Guillaume <guillaume@pelikan-mobility.com>
2025-12-01 10:33:41 +0100

    day 1 part 2

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -3,18 +3,28 @@ use std::io::{self, BufRead};
 fn main() -> anyhow::Result<()> {
     let mut cur = 50;
     let mut nb_zero = 0;
+    let mut nb_pass_zero = 0;
     for l in io::BufReader::new(std::fs::File::open("data/example1.txt")?).lines() {
         let l = l?;
         let n = l[1..].parse::<i32>()?;
         match l.chars().next() {
-            Some('R') => cur = (cur + n) % 100,
-            Some('L') => cur = (cur + 100 - n) % 100,
+            Some('R') => {
+                for _ in 0..n {
+                    cur = (cur + 1) % 100;
+                    nb_pass_zero += i32::from(cur == 0);
+                }
+            }
+            Some('L') => {
+                for _ in 0..n {
+                    cur = (cur + 100 - 1) % 100;
+                    nb_pass_zero += i32::from(cur == 0);
+                }
+            }
             _ => anyhow::bail!("bad line {l}"),
         }
-        if cur == 0 {
-            nb_zero += 1;
-        }
+        nb_zero += i32::from(cur == 0);
     }
     println!("Part1: {}", nb_zero);
+    println!("Part2: {}", nb_pass_zero);
     Ok(())
 }
