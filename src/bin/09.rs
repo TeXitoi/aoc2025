522fc9e3b03796cdf1c1b19e2b099c5309bdc736
Guillaume <guillaume@pelikan-mobility.com>
2025-12-09 17:45:21 +0100

    Day 9: with Rect struct

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,30 +1,29 @@
 type Coord = (i64, i64);
-fn transpose((x, y): Coord) -> Coord {
-    (y, x)
+#[derive(Copy, Clone)]
+struct Rect {
+    min_x: i64,
+    min_y: i64,
+    max_x: i64,
+    max_y: i64,
 }
-fn intersect(mut r: (Coord, Coord), mut s: (Coord, Coord)) -> bool {
-    if s.0.1 == s.1.1 {
-        r = (transpose(r.0), transpose(r.1));
-        s = (transpose(s.0), transpose(s.1));
+impl Rect {
+    fn new(coords: &[Coord]) -> Self {
+        Self {
+            min_x: coords[0].0.min(coords[1].0),
+            min_y: coords[0].1.min(coords[1].1),
+            max_x: coords[0].0.max(coords[1].0),
+            max_y: coords[0].1.max(coords[1].1),
+        }
     }
-    let min_r_x = r.0.0.min(r.1.0);
-    let max_r_x = r.0.0.max(r.1.0);
-    let min_r_y = r.0.1.min(r.1.1);
-    let max_r_y = r.0.1.max(r.1.1);
-    let s_x = s.0.0;
-    let min_s_y = s.0.1.min(s.1.1);
-    let max_s_y = s.0.1.max(s.1.1);
-    if s_x <= min_r_x || max_r_x <= s_x {
-        return false;
+    fn intersect(self, other: Self) -> bool {
+        !(self.max_x <= other.min_x
+            || other.max_x <= self.min_x
+            || self.max_y <= other.min_y
+            || other.max_y <= self.min_y)
     }
-    if max_s_y <= min_r_y || max_r_y <= min_s_y {
-        return false;
+    fn area(self) -> i64 {
+        (self.max_x - self.min_x + 1) * (self.max_y - self.min_y + 1)
     }
-    true
-}
-fn has_segment_intersecting_rect(r: (Coord, Coord), coords: &[Coord]) -> bool {
-    coords.windows(2).any(|s| intersect(r, (s[0], s[1])))
-        || intersect(r, (coords[0], coords[coords.len() - 1]))
 }
 fn main() -> anyhow::Result<()> {
     let coords = std::fs::read_to_string("data/example09.txt")?
@@ -36,13 +35,18 @@ fn main() -> anyhow::Result<()> {
             Ok((x.parse()?, y.parse()?))
         })
         .collect::<anyhow::Result<Vec<Coord>>>()?;
+    let segments: Vec<_> = coords
+        .windows(2)
+        .chain([[coords[0], coords[coords.len() - 1]].as_slice()])
+        .map(Rect::new)
+        .collect();
     for i in [1, 2] {
         let max = coords
             .iter()
             .enumerate()
-            .flat_map(|(i, &c1)| coords[i + 1..].iter().map(move |&c2| (c1, c2)))
-            .filter(|&r| i == 1 || !has_segment_intersecting_rect(r, &coords))
-            .map(|(c1, c2)| ((c1.0 - c2.0).abs() + 1) * ((c1.1 - c2.1).abs() + 1))
+            .flat_map(|(i, &c1)| coords[i + 1..].iter().map(move |&c2| Rect::new(&[c1, c2])))
+            .filter(|&r| i == 1 || segments.iter().all(|&segment| !r.intersect(segment)))
+            .map(|r| r.area())
             .max()
             .unwrap_or(0);
         println!("Part{i}: {max}");
