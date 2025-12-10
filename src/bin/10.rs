157b52aa2d3d1ed8a0a8eb367fe00258b9be5b86
Guillaume <guillaume@pelikan-mobility.com>
2025-12-10 19:59:31 +0100

    Day 10 part 2 with LP

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,31 +1,31 @@
+use std::collections::BTreeSet;
+
 #[derive(Debug)]
 struct Machine {
-    lights: u32,
-    buttons: Vec<u32>,
-    joltages: Vec<i64>,
+    lights: Vec<bool>,
+    buttons: Vec<Vec<usize>>,
+    joltages: Vec<i32>,
 }
 impl Machine {
     fn new(s: &str) -> anyhow::Result<Self> {
         let Some((l, q)) = s.split_once(' ') else {
             anyhow::bail!("bad line {s:?}")
         };
-        let lights = l.chars().rev().fold(0, |accu, c| match c {
-            '.' => accu << 1,
-            '#' => (accu << 1) + 1,
-            _ => accu,
-        });
+        let lights = l
+            .chars()
+            .filter_map(|c| match c {
+                '.' => Some(false),
+                '#' => Some(true),
+                _ => None,
+            })
+            .collect();
         let Some((bs, js)) = q.rsplit_once(' ') else {
             anyhow::bail!("bad line {s:?}")
         };
         let buttons = bs
             .split(' ')
-            .map(|b| {
-                b[1..b.len() - 1]
-                    .split(',')
-                    .map(|l| l.parse().unwrap())
-                    .fold(0, |accu, b: u32| accu | (1 << b))
-            })
-            .collect();
+            .map(|b| b[1..b.len() - 1].split(',').map(|l| l.parse()).collect())
+            .collect::<Result<Vec<Vec<_>>, _>>()?;
         let joltages = js[1..js.len() - 1]
             .split(',')
             .map(|l| l.parse())
@@ -36,37 +36,75 @@ impl Machine {
             joltages,
         })
     }
-    fn check_lights(&self, mut press: u32) -> bool {
-        //println!("press  {:10b}", press);
-        let mut lights = 0;
-        for i in 0.. {
-            if press == 0 {
-                break;
+    fn search(&self) -> Option<u32> {
+        let mut nodes = BTreeSet::from([(0, 0, vec![0; self.lights.len()])]);
+        let mut last = 0;
+        let mut seen = BTreeSet::default();
+        while let Some((num, _, joltages)) = nodes.pop_first() {
+            if last != num {
+                last = num;
+                dbg!(num);
+                dbg!(nodes.len());
+                dbg!(seen.len());
             }
-            if press & 1 == 1 {
-                lights ^= self.buttons[i];
+            for button in &self.buttons {
+                let mut joltages = joltages.to_vec();
+                for &b in button {
+                    joltages[b] += 1;
+                }
+                match self.check_joltages(&joltages) {
+                    Some(true) => return Some(num + 1),
+                    Some(false) => {}
+                    None => {
+                        if seen.insert(joltages.clone()) {
+                            nodes.insert((num + 1, -joltages.iter().sum::<i32>(), joltages));
+                        }
+                    }
+                }
             }
-            press >>= 1;
         }
-        //println!("lights {:10b}", lights);
-        lights == self.lights
+        None
     }
-    fn num_buttons(&self) -> Option<u32> {
-        //println!("lights {:10b}", self.lights);
-        let max = (1 << self.buttons.len()) - 1;
-        //println!("max    {:10b}", max);
-        (0u32..max)
-            .filter(|&bs| self.check_lights(bs))
-            .map(|bs| bs.count_ones())
-            .min()
+    fn check_joltages(&self, joltages: &[i32]) -> Option<bool> {
+        if joltages == self.joltages {
+            Some(true)
+        } else if self.joltages.iter().zip(joltages).all(|(r, cur)| cur <= r) {
+            None
+        } else {
+            Some(false)
+        }
+    }
+    fn solve(&self) -> Option<u32> {
+        use good_lp::{Expression, ProblemVariables, Solution, SolverModel, variable};
+        let mut problem = ProblemVariables::new();
+        let variables: Vec<_> = self
+            .buttons
+            .iter()
+            .map(|_| problem.add(variable().integer().min(0)))
+            .collect();
+        let objective = variables.iter().sum::<Expression>();
+        let mut problem = problem.minimise(&objective).using(good_lp::highs);
+        for (i, &target) in self.joltages.iter().enumerate() {
+            problem.add_constraint(
+                self.buttons
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, b)| b.contains(&i))
+                    .map(|(i, _)| variables[i])
+                    .sum::<Expression>()
+                    .eq(target),
+            );
+        }
+        let solution = problem.solve().unwrap();
+        Some(solution.eval(objective).round() as u32)
     }
 }
 fn main() -> anyhow::Result<()> {
-    let machines = std::fs::read_to_string("data/example10.txt")?
+    let mut machines = std::fs::read_to_string("data/input10.txt")?
         .lines()
         .map(|l| Machine::new(l))
         .collect::<anyhow::Result<Vec<_>>>()?;
-    let num_buttons = machines.iter().filter_map(|m| m.num_buttons()).sum::<u32>();
-    println!("Part1: {num_buttons}");
+    let num: u32 = machines.iter_mut().map(|m| dbg!(m.solve().unwrap())).sum();
+    println!("Part2: {num}");
     Ok(())
 }
